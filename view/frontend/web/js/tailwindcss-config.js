1a637652443a5a287abd1f18733227286eb85c84
Apti <apachethank@gmail.com>
2023-12-25 14:25:19 +0100

    Tests and code format

---
+++ view/frontend/web/js/tailwindcss-config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        'app/code/Dnd/Offer/../**/*.phtml',
+        'app/code/Dnd/Offer/../**/*.html',
+        'app/code/Dnd/Offer/../**/*.js'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
